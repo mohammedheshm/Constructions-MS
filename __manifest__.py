dd47ae0df39ac1c828fb63e4efb52115a9c99623
Mohammed Hesham <mohammedhe4am@gmail.com>
2026-03-26 17:55:28 +0200

    first commit

---
+++ __manifest__.py
@@ -0,0 +1,15 @@
+{
+    'name': "Construction Management",
+    'author': "Mohammed Hesham",
+    'category': '',
+    'version': '17.0.0.1.0',
+    'depends': ['base', 'contacts', 'account'],
+    'data': [
+        'security/ir.model.access.csv',
+        'data/sequence.xml',
+        'views/construction_menu.xml',
+        'views/construction_project_view.xml',
+    ],
+
+    'application': True,
+}
---
+++ models/construction_project.py
@@ -0,0 +1,51 @@
+from odoo import api, fields, models, exceptions
+from odoo.exceptions import ValidationError
+
+
+class ConstructionProject(models.Model):
+    _name = 'construction.project'
+    _description = 'Projects'
+
+    project_name = fields.Char(string='Project Name', required=True)
+    project_code = fields.Char(default='New', string='Project Code', readonly=True)
+    customer = fields.Char(string='Customer')
+    project_manager = fields.Many2one('res.users', string='Project Manager')
+    start_date = fields.Date(string='Start Date', required=True)
+    end_date = fields.Date(string='End Date', required=True)
+    budget = fields.Float(string='Budget')
+    state = fields.Selection([
+        ('draft', 'Draft'),
+        ('in_progress', 'In Progress'),
+        ('done', 'Done'),
+        ('closed', 'Closed'),
+    ], string='State', default='draft', required=True)
+    description = fields.Text(string='Description', required=True)
+
+    def action_draft(self):
+        for rec in self:
+            rec.state = 'draft'
+
+    def action_in_progress(self):
+        for rec in self:
+            rec.state = 'in_progress'
+
+    def action_done(self):
+        for rec in self:
+            if rec.state != 'done':
+                rec.state = 'done'
+
+    def action_close(self):
+        for rec in self:
+            rec.state = 'closed'
+
+    @api.model
+    def create(self, vals):
+        if vals.get('project_code', 'New') == 'New':
+            vals['project_code'] = self.env['ir.sequence'].next_by_code('Project_seq') or 'New'
+        return super(ConstructionProject, self).create(vals)
+
+    @api.constrains('start_date', 'end_date')
+    def _check_dates(self):
+        for rec in self:
+            if rec.end_date < rec.start_date:
+                raise ValidationError('End Date must be after Start Date!')
